4b1b5ba0c161199d571d908ef91826da8b3c8ef1
Mohamed <mohamedk@kth.se>
2024-02-12 00:04:02 +0100

    combined code2  feb-12

--- src/App.js
+++ src/App.js
@@ -3,27 +3,31 @@ import Home from "./views/HomeView";
 import LoginView from './views/LoginView';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import AuthProvider from "./hooks/AuthProvider";
+import HeaderView from "./views/HeaderView";
+import SignUpView from "./views/SignUpView";
 
-
-
-// Define the main App component
 function App() {
   return (
     <div className="App">
-
-      {
-        <Router>
-          <AuthProvider>
-            <Routes>
-              <Route path="/" element={<LoginView/>}/>
-              <Route path="/hello" element={<Home/>}/>
-            </Routes>
-          </AuthProvider>
-        </Router>
-
-
-      }
-
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route
+              path="*"
+              element={
+                <div>
+                  <HeaderView />
+                  <Routes>
+                  <Route path="/signup" element={<SignUpView />} />
+                  <Route path="/" element={<LoginView />} />
+                  <Route path="/hello" element={<Home />} />
+                  </Routes>
+                </div>
+              }
+            />
+          </Routes>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
---
+++ src/views/SignUpView.js
@@ -0,0 +1,105 @@
+import '../styling/signUpView.css';
+import React, { useState } from 'react';
+import DataSource from '../hooks/dataSource';
+import { useNavigate } from 'react-router-dom';
+
+function SignUpView(props) {
+  const navigate = useNavigate();
+
+  const [error, setError] = useState('');
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [pnr, setPnr] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = () => {
+     setError('');
+     if (!name || !surname || !pnr || !email || !username || !password) {
+         setError('All fields are required');
+          return;
+      }
+
+     if(!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).test(email)){
+       setError('Invalid email format');
+      return;
+     }
+      
+      const signUpData = { name, surname, pnr, email, username, password };
+  
+      DataSource.registerUser(signUpData)
+      .then(response => {
+       console.log('Registration successful:', response);
+        navigate('/');
+        return response;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        setError(error.toString().substring(6));
+      });
+  };
+
+
+  return (
+    <div className="signup-section">
+      <form className="signup-container" onSubmit={(e) => e.preventDefault()}>
+        <div className="input-group">
+          <div className="input-row">
+            <input
+              placeholder="NAME"
+              type="text"
+              className="input-box"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              placeholder="SURNAME"
+              type="text"
+              className="input-box"
+              onChange={(e) => setSurname(e.target.value)}
+            />
+          </div>
+          <div className="input-row">
+            <input
+              placeholder="PERSONAL NUMBER"
+              type="text"
+              className="input-box"
+              onChange={(e) => setPnr(e.target.value)}
+            />
+          </div>
+          <div className="input-row">
+            <input
+              placeholder="USERNAME"
+              type="text"
+              className="input-box"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="input-row">
+            <input
+              placeholder="EMAIL"
+              type="email"
+              className="input-box"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-row">
+            <input
+              placeholder="PASSWORD"
+              className="input-box"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="new-password"
+            />
+          </div>
+        </div>
+        <p className="message-error">{error}</p>
+        <button className="login-button" onClick={handleSignUp}>
+          REGISTER
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUpView;
